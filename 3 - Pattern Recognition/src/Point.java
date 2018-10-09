23b87fa518bdd46dd6aea82e90dc0c9fabece0e1
daulet.urazalinov <daulet.urazalinov@uptake.com>
2018-10-09 10:08:37 -0400

    Add Pattern Recognition code

---
+++ 3 - Pattern Recognition/src/Point.java
@@ -0,0 +1,57 @@
+import java.util.Comparator;
+import edu.princeton.cs.algs4.StdDraw;
+
+public class Point implements Comparable<Point> {
+    private final int x;
+    private final int y;
+
+    // constructs the point (x, y)
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // draws this point
+    public void draw() {
+        StdDraw.point(x, y);
+    }
+
+    // draws the line segment from this point to that point
+    public void drawTo(Point that) {
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    // compare two points by y-coordinates, breaking ties by x-coordinates
+    public int compareTo(Point that) {
+        if (this.y == that.y) return this.x - that.x;
+
+        return this.y - that.y;
+    }
+
+    // the slope between this point and that point
+    public double slopeTo(Point that) {
+        return (that.y - this.y)/(double)(that.x - this.x);
+    }
+
+    // compare two points by slopes they make with this point
+    public Comparator<Point> slopeOrder() {
+        return new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                return (int)(slopeTo(o1) - slopeTo(o2));
+            }
+        };
+    }
+
+    /**
+     * Unit tests the Point data type.
+     */
+    public static void main(String[] args) {
+        /* YOUR CODE HERE */
+    }
+}
